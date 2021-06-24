87aaf877f3a18977772e6f2241be09f552310d31
Skyline-23 <bs2740@naver.com>
2021-06-24 22:13:35 +0900

    [ADD] 결과 창 구현

---
+++ src/components/ResultList.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <div class="resultList">
+            {{ time }}
+        </div>
+</div>
+</template>
+<script>
+export default {
+    name: 'ResultList',
+    props: ['time']
+}
+</script>
+<style lang="scss">
+.resultList {
+    margin: 10px 0;
+    padding: 10px 5px;
+    border: 1px solid #ddd;
+}
+</style>
--- src/views/Result.vue
+++ src/views/Result.vue
@@ -1,5 +1,39 @@
 <template>
     <div class="result">
-        
+        <div class="result-title">
+            Your Score
+        </div>
+        <ResultList v-for="time, index in $root.times" :key="index" v-bind:time="time" />
+        <router-link to='/'>
+            <button class="result-btn">home</button>
+        </router-link>
     </div>
 </template>
+
+<script>
+import ResultList from '../components/ResultList.vue';
+
+export default {
+    name: 'Result',
+    components: {ResultList},
+}
+</script>
+<style lang='scss'>
+.result {
+    width: 300px;
+    margin: 100px auto;
+
+    &-title {
+        padding: 10px;
+        text-align: center;
+        color: white;
+        background-color: #000000;
+    }
+    &-btn {
+        width: 100%;
+        border: none;
+        padding: 10px;
+        background-color: #ddd;
+    }
+}
+</style>
