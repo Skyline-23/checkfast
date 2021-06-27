ccc1bbc8735a941bbff9f4a383b7e1e33b15b255
ivory0312 <63035523+ivory0312@users.noreply.github.com>
2021-06-27 21:04:29 +0900

    [ADD] css

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,39 +3,53 @@
     <div class="title">
       스피드 테스트
     </div>
-    <div>
-    <router-link to="/wait">
-      <button class="Start-button">시작하기</button>
-    </router-link>
+    <div class="explain">
+      시작하기 버튼을 누르시고 화면이 검정색으로 바뀔때 최대한 빠르게 마우스를
+      클릭해보세요!
+    </div>
+    <div class="bottom">
+      <router-link to="/wait">
+        <button class="button">
+          시작하기
+        </button>
+      </router-link>
     </div>
   </div>
 </template>
 
 <style lang="css" scoped>
-.Start-button{
-  float: left;
-  margin-top: 300px;
-  width: 300px;
-  height: 30px;
-  border: 0px;
-  border-radius: 50px;
-  background: linear-gradient(145deg, #cacaca, #f0f0f0);
-  box-shadow:  44px 44px 100px #c5c5c5,
-  -44px -44px 100px #fbfbfb;
+.home {
+  width: 700px;
+  margin: 200px auto;
 }
 .title {
-  width: 738px;
-  height: 183px;
-  float: left;
-  background: linear-gradient(270deg, #00D1FF 0%, rgba(57, 207, 126, 0.66) 100%);
-  border-radius: 26px;
+  width: 100%;
+  height: 180px;
+  font-size: 80px;
+  font-weight: bold;
   display: flex;
   justify-content: center;
+  align-items: center;
+  color: white;
+  border-radius: 50px 50px 0 0;
+  background-color: black;
+}
+.explain {
+  width: 698px;
+  height: 150px;
+  display: flex;
+  font-size: 18px;
+  justify-content: center;
+  border: 1px solid black;
   align-items: center;
 }
-.home{
-  display: flex;
-  justify-content: center;
-  align-items: center;
+.button {
+  width: 100%;
+  height: 50px;
+  font-size: 24px;
+  border: none;
+  border-radius: 0 0 50px 50px;
+  color: white;
+  background-color: black;
 }
 </style>
